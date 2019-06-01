393f18afe5e3799f5fd9ab6bb32a60a8c2c4f7c2
groceryheist <groceryheist@gmail.com>
2019-06-01 19:00:49 -0400

    create setup.py

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pyRemembeR',
+      version='0.1',
+      description='A simple utility for data scientists to save intermediate objects from either R or python.',
+      utl='http://github.com/groceryheist/pyRemembeR',
+      author='Nate TeBlunthuis',
+      author_email='nathante@uw.edu',
+      license='GPL3',
+      packages=['pyRemembeR'],
+      zip_safe=False)
+
