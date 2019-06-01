61f3363f35e8bdee2a5cb97ed5e6902f590da7af
groceryheist <nathante@uw.edu>
2019-06-01 17:58:45 -0400

    pin minimum version of rpy2

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(name='pyRemembeR',
-      version='0.1.01',
+      version='0.1.2',
       description='A simple utility for data scientists to save intermediate objects from either R or python.',
       utl='http://github.com/groceryheist/pyRemembeR',
       author='Nate TeBlunthuis',
       author_email='nathante@uw.edu',
       license='GPL3',
       packages=['pyRemembeR'],
-      install_requires[
-          'rpy2',
-      ]
+      install_requires=[
+          'rpy2>=3.04',
+      ],
       zip_safe=False)
 
