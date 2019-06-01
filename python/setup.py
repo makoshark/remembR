ba3e1fe0558c5fa166660b568f99d995a4a2d36e
groceryheist <nathante@uw.edu>
2019-06-01 17:41:54 -0400

    Add dependency on rpy2

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
 setup(name='pyRemembeR',
-      version='0.1',
+      version='0.1.01',
       description='A simple utility for data scientists to save intermediate objects from either R or python.',
       utl='http://github.com/groceryheist/pyRemembeR',
       author='Nate TeBlunthuis',
       author_email='nathante@uw.edu',
       license='GPL3',
       packages=['pyRemembeR'],
+      install_requires[
+          'rpy2',
+      ]
       zip_safe=False)
 
