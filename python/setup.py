1b8da5ea3a291dca1077035da508e76dda8d7921
groceryheist <nathante@uw.edu>
2019-06-23 19:43:12 -0700

    up version

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyRemembeR',
-      version='0.1.8',
+      version='0.1.9',
       description='A simple utility for data scientists to save intermediate objects from either R or python.',
       utl='http://github.com/groceryheist/pyRemembeR',
       author='Nate TeBlunthuis',
@@ -10,7 +10,8 @@ setup(name='pyRemembeR',
       packages=['pyRemembeR'],
       install_requires=[
           'rpy2>=3.0.4',
-          'filelock>=3.0.12'
+          'filelock>=3.0.12',
+          'feather-format>=0.4.0'
       ],
       zip_safe=False)
 
